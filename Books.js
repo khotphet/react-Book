1b06ef5a39ecba412f938cb771db03d39fe8e5f0
khotphet <profess538@gmail.com>
2020-06-16 17:31:10 +0700

    1 file modified

--- Books.js
+++ Books.js
@@ -20,7 +20,8 @@ class Books extends Component {
             .query({ q: this.state.searchField })
             .then((data) => {
             console.log(data);
-            this.setState({ books: [...data.body.items] })
+            const cleanData = this.cleanData(data)
+            this.setState({ books: cleanData })
       })
     }
     handleSort = (e) =>{
@@ -36,9 +37,27 @@ class Books extends Component {
     }
 
     cleanData = (e) =>{
-
+      const cleanedData = data.body.items.map((book) =>{
+        if (book.volumeInfo.hasOwnProperty('publishedDate')=== false){
+          book.volumeInfo['publishedDate'] = '0000';
+        }
+        else if (book.volumeInfo.hasOwnProperty('imageLinks')=== false){
+          book.volumeInfo['imageLinks'] = {thumbnail: ''};
+        }
+      })
+      return book;
     }
   render() {
+    const sortedBooks = this.state.books.sort((a,b)=>{
+      if(this.state.sort === 'Newest'){
+        return parseInt(b.volumeInfo.publishedDate.substring(0,4))
+        - parseInt(a.volumeInfo.publishedDate.substring(0,4))
+      }
+      else if(this.state.sort === 'Oldest'){
+        return parseInt(b.volumeInfo.publishedDate.substring(0,4))
+        - parseInt(b.volumeInfo.publishedDate.substring(0,4))
+      }
+    })
     return (
       <div>
         <SearchArea searchBook={this.searchBook} handleSearch={this.handleSearch} handleSort={this.handleSort}/>
